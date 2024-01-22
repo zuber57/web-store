29b11710bccad1edf53c83ac403f418e87abcc59
zuber57 <zubarahmad48@gmail.com>
2024-01-22 19:47:57 +0530

    -

---
+++ index.js
@@ -0,0 +1,5 @@
+// const data = document.querySelector('.selling_items');
+$('.selling_items').click(function(){
+   var filter_value  =  $(this).attr('filter-atr');
+   console.log(filter_value)
+})
